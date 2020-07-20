49e2b642f529ae1b46a266ad87b23845d42a2cbd
cyber-venom003 <tech.worm.1728@gmail.com>
2020-07-20 07:41:46 +0530

    Production Build Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: "./src/app.js",
-    mode: "development",
-    output: {
-        path: path.join(__dirname , "public"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        } , {
-            test: /\.s?css$/,
-            use: ["style-loader" , "css-loader" , "sass-loader"]
-        }]
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: path.join(__dirname , 'public'),
-        historyApiFallback: true
-    }
-}
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    return {
+        entry: "./src/app.js",
+        mode: "development",
+        output: {
+            path: path.join(__dirname , "public"),
+            filename: "bundle.js"
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            } , {
+                test: /\.s?css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader' , 'sass-loader'],
+            }]
+        },
+        plugins: [new MiniCssExtractPlugin()],
+        devtool: isProduction? 'source-map' : 'eval-cheap-module-source-map',
+        devServer: {
+            contentBase: path.join(__dirname , 'public')
+        }
+    };
+};
